52cbddce2d15d44619a3fe4a01cd5bced54e5e09
MhNirra <72897503+MhNirra@users.noreply.github.com>
2023-05-03 20:40:43 -0400

    Add files via upload

---
+++ Ordenamiento por seleccion/porSeleccion.cpp
@@ -0,0 +1,33 @@
+/*
+Por Seleccion
+*/
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+
+	// Algoritmo del ordenamiento pro seleccion
+	int numeros[] = { 3,2,1,5,4 };
+	int i, j, aux, min;
+
+	for (i = 0; i < 5; i++) {
+		min = i;
+		for (j = i + 1; i < 5; i++) {
+			if (numeros[j] < numeros[min]) {
+				min = j;
+			}
+		}
+		aux = numeros[i];
+		numeros[i] = numeros[min];
+		numeros[min] = aux;
+	}
+	cout << "Orden ascentente; ";
+	for (i = 0; i < 5; i++) {
+		cout << numeros[i] << " ";
+	}
+
+	_getch();
+}
